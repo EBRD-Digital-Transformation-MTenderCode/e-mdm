90b9c49ca9b135f84a0031f54e7cf4bda00b4ac3
andreykolchev <andreykolchev@gmail.com>
2017-10-02 15:48:35 +0300

    enable swagger

--- src/main/java/com/procurement/mdm/MdmApplication.java
+++ src/main/java/com/procurement/mdm/MdmApplication.java
@@ -3,8 +3,10 @@ package com.procurement.mdm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {LiquibaseAutoConfiguration.class})
+@EnableSwagger2
 public class MdmApplication {
     public static void main(String[] args) {
         SpringApplication.run(MdmApplication.class, args);
