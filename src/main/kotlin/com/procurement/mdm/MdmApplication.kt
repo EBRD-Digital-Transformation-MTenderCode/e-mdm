7f98cb8cd1c34024359d39dfcc07031ebe6a839e
andreykolchev <andreykolchev@gmail.com>
2018-06-19 18:38:44 +0300

    add cloud and docker build

--- src/main/kotlin/com/procurement/mdm/MdmApplication.kt
+++ src/main/kotlin/com/procurement/mdm/MdmApplication.kt
@@ -3,8 +3,10 @@ package com.procurement.mdm
 import com.procurement.mdm.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
+@EnableEurekaClient
 class MdmApplication
 
 fun main(args: Array<String>) {
