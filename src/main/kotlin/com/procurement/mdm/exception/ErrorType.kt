95bc0646c65df3ee18f1e6437d1194ea3e510748
andreykolchev <andreykolchev@gmail.com>
2018-06-21 14:23:25 +0300

    add cloud and docker build

--- src/main/kotlin/com/procurement/mdm/exception/ErrorType.kt
+++ src/main/kotlin/com/procurement/mdm/exception/ErrorType.kt
@@ -2,7 +2,7 @@ package com.procurement.mdm.exception
 
 enum class ErrorType constructor(val code: String, val message: String) {
     LANG_UNKNOWN("00.01", "Language not found."),
-    COUNTRY_UNKNOWN("00.02", "Country not found."),
+    COUNTRY_UNKNOWN("00.02", "Country for current language not found."),
     UNIT_CLASS_UNKNOWN("00.03", "Unit class not found."),
     ENTITY_KIND_UNKNOWN("00.03", "Entity kind not found.");
 }
