539f7c2df3b36eb7e97ff1028a9ab4bf6efb4c93
andreykolchev <andreykolchev@gmail.com>
2018-06-21 13:47:04 +0300

    add cloud and docker build

--- src/main/kotlin/com/procurement/mdm/controller/CPVController.kt
+++ src/main/kotlin/com/procurement/mdm/controller/CPVController.kt
@@ -4,12 +4,10 @@ import com.procurement.mdm.model.bpe.ResponseDto
 import com.procurement.mdm.service.CPVService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(maxAge = 3600)
 @RequestMapping("/cpv")
 class CPVController(private val cpvService: CPVService) {
 
