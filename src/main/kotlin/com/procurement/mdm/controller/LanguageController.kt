37fb48e23355513069ae5f203b7aa54dbfbb54cb
andreykolchev <andreykolchev@gmail.com>
2018-06-23 10:08:52 +0300

    add id

--- src/main/kotlin/com/procurement/mdm/controller/LanguageController.kt
+++ src/main/kotlin/com/procurement/mdm/controller/LanguageController.kt
@@ -1,20 +1,20 @@
-//package com.procurement.mdm.controller
-//
-//import com.procurement.mdm.model.dto.ResponseDto
-//import com.procurement.mdm.service.LanguageService
-//import org.springframework.http.HttpStatus
-//import org.springframework.http.ResponseEntity
-//import org.springframework.web.bind.annotation.*
-//
-//@RestController
-//@CrossOrigin(maxAge = 3600)
-//@RequestMapping("/language")
-//class LanguageController(private val languageService: LanguageService) {
-//
-//    @GetMapping
-//    fun getLanguages(@RequestParam(required = false) internal: Boolean = false): ResponseEntity<ResponseDto> {
-//        return ResponseEntity(
-//                languageService.getLanguages(internal),
-//                HttpStatus.OK)
-//    }
-//}
+package com.procurement.mdm.controller
+
+import com.procurement.mdm.model.dto.ResponseDto
+import com.procurement.mdm.service.LanguageService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@CrossOrigin(maxAge = 3600)
+@RequestMapping("/language")
+class LanguageController(private val languageService: LanguageService) {
+
+    @GetMapping
+    fun getLanguages(@RequestParam(required = false) internal: Boolean = false): ResponseEntity<ResponseDto> {
+        return ResponseEntity(
+                languageService.getLanguages(internal),
+                HttpStatus.OK)
+    }
+}
