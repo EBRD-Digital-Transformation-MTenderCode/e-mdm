547019d4229f0c7c878122385d243b2a739c9bc0
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-09-20 13:02:08 +0300

    Removed Eureka client

--- src/main/kotlin/com/procurement/mdm/MdmApplication.kt
+++ src/main/kotlin/com/procurement/mdm/MdmApplication.kt
@@ -3,10 +3,8 @@ package com.procurement.mdm
 import com.procurement.mdm.config.ApplicationConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication(scanBasePackageClasses = [ApplicationConfig::class])
-@EnableEurekaClient
 class MdmApplication
 
 fun main(args: Array<String>) {
