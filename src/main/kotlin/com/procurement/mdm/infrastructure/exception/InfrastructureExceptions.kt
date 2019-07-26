248d9a9aab4b7712e5a50910ed1a7d21c216402b
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-07-26 17:48:26 +0300

    Changed description for exception LanguageRequestParameterMissingException.

--- src/main/kotlin/com/procurement/mdm/infrastructure/exception/InfrastructureExceptions.kt
+++ src/main/kotlin/com/procurement/mdm/infrastructure/exception/InfrastructureExceptions.kt
@@ -3,7 +3,7 @@ package com.procurement.mdm.infrastructure.exception
 sealed class InfrastructureException(val description: String) : RuntimeException(description)
 
 class LanguageRequestParameterMissingException :
-    InfrastructureException("The request is missing a required query parameter - 'language'.")
+    InfrastructureException("The request is missing a required query parameter - 'lang'.")
 
 class CountryRequestParameterMissingException :
     InfrastructureException("The request is missing a required query parameter - 'country'.")
