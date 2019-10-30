9f25d1784fb28193de3397c44c5bb718a8cc9af1
Evgen Kopach <zkopach@gmail.com>
2019-10-30 11:42:22 +0200

    ES-2069 Updated data model for operation 'processBid'

--- src/main/kotlin/com/procurement/mdm/model/dto/data/OrganizationReference.kt
+++ src/main/kotlin/com/procurement/mdm/model/dto/data/OrganizationReference.kt
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import com.procurement.mdm.model.dto.databinding.JsonDateDeserializer
 import com.procurement.mdm.model.dto.databinding.JsonDateSerializer
 import java.time.LocalDateTime
-import java.util.*
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class OrganizationReference @JsonCreator constructor(
@@ -108,15 +107,97 @@ data class ContactPoint @JsonCreator constructor(
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Details @JsonCreator constructor(
 
+        val typeOfSupplier: String,
+
+        val mainEconomicActivities: List<String>,
+
         val typeOfBuyer: TypeOfBuyer?,
 
         val mainGeneralActivity: MainGeneralActivity?,
 
         val mainSectoralActivity: MainSectoralActivity?,
 
-        val scale: Scale?
+        val scale: Scale?,
+
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val permits: List<Permit>?,
+
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val bankAccounts: List<BankAccount>?,
+
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val legalForm: LegalForm?
 )
 
+data class Permit(
+        val id: String,
+        val scheme: String,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val url: String?,
+
+        val permitDetails: PermitDetails
+)
+
+data class PermitDetails(
+        val issuedBy: IssuedBy,
+        val issuedThought: IssuedThought,
+        val validityPeriod: ValidityPeriod
+)
+
+
+data class IssuedBy(
+        val id: String,
+        val name: String
+)
+
+data class IssuedThought(
+        val id: String,
+        val name: String
+)
+
+data class ValidityPeriod(
+        val startDate: LocalDateTime,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val endDate: LocalDateTime?
+)
+
+data class BankAccount(
+        val description: String,
+        val bankName: String,
+        val address: Address,
+        val identifier: Identifier,
+        val accountIdentification: AccountIdentification,
+
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        val additionalAccountIdentifiers: List<AdditionalAccountIdentifier>?
+) {
+        data class Identifier(
+                val scheme: String,
+                val id: String
+        )
+
+        data class AdditionalAccountIdentifier(
+                val id: String,
+                val scheme: String
+        )
+
+        data class AccountIdentification(
+                val scheme: String,
+                val id: String
+        )
+}
+
+
+data class LegalForm(
+        val scheme: String,
+        val id: String,
+        val description: String,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        val uri: String?
+)
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Address @JsonCreator constructor(
