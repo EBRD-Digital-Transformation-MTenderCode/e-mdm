ff897eb2b12b0b04b40bf18bcc5ce03a8caed2f4
Maxim Sambulat <maxim.sambulat@gmail.com>
2019-12-20 12:31:00 +0200

    ES-2423 Added availability of processing the address of the bidder from another country.

--- src/main/kotlin/com/procurement/mdm/service/preparation/EnquiryDataService.kt
+++ src/main/kotlin/com/procurement/mdm/service/preparation/EnquiryDataService.kt
@@ -1,6 +1,5 @@
 package com.procurement.mdm.service.preparation
 
-import com.procurement.mdm.utils.toObject
 import com.procurement.mdm.exception.ErrorType
 import com.procurement.mdm.exception.InErrorException
 import com.procurement.mdm.model.dto.CommandMessage
@@ -8,6 +7,7 @@ import com.procurement.mdm.model.dto.ResponseDto
 import com.procurement.mdm.model.dto.data.EnquiryData
 import com.procurement.mdm.model.dto.getResponseDto
 import com.procurement.mdm.service.ValidationService
+import com.procurement.mdm.utils.toObject
 import org.springframework.stereotype.Service
 
 interface EnquiryDataService {
@@ -16,23 +16,24 @@ interface EnquiryDataService {
 }
 
 @Service
-class EnquiryDataServiceImpl(private val validationService: ValidationService,
-                             private val organizationDataService: OrganizationDataService
+class EnquiryDataServiceImpl(
+    private val validationService: ValidationService,
+    private val organizationDataService: OrganizationDataService
 ) : EnquiryDataService {
 
     override fun processEnquiryData(cm: CommandMessage): ResponseDto {
         val lang = cm.context.language
-        val country = validationService.getCountry(languageCode = lang, countryCode = cm.context.country)
         val dto = getData(cm)
         val author = dto.author
+        val countryCode = author.address!!.addressDetails.country.id
+        val country = validationService.getCountry(languageCode = lang, countryCode = countryCode)
         organizationDataService.processOrganization(author, country)
         return getResponseDto(data = dto, id = cm.id)
     }
 
-    private fun getData(cm: CommandMessage): EnquiryData {
-        if (cm.data.size() == 0) throw InErrorException(ErrorType.INVALID_DATA, null, cm.id)
-        return toObject(EnquiryData::class.java, cm.data)
-    }
-
+    private fun getData(cm: CommandMessage): EnquiryData =
+        if (cm.data.size() != 0)
+            toObject(EnquiryData::class.java, cm.data)
+        else
+            throw InErrorException(ErrorType.INVALID_DATA, null, cm.id)
 }
-
