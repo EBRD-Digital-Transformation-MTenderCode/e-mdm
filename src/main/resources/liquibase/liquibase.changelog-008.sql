9709cfa3c7cd25f492fd0b85b7c939a1910bbd9b
katherine <kate.shaidurova@gmail.com>
2020-06-10 18:35:53 +0300

    ES-3433 Renamed countries table to country_schemes and countries_i18n to country_schemes_i18n

--- src/main/resources/liquibase/liquibase.changelog-008.sql
+++ src/main/resources/liquibase/liquibase.changelog-008.sql
@@ -1,29 +1,29 @@
 --liquibase formatted sql
 
 --changeset kateshaidurova:countries
-CREATE TABLE public.countries (
+CREATE TABLE public.country_schemes (
   id              BIGINT     NOT NULL PRIMARY KEY,
   list_schemes_id BIGINT     NOT NULL REFERENCES list_schemes (id),
   code            VARCHAR(2) NOT NULL,
   UNIQUE (list_schemes_id, code)
 );
 
---changeset kateshaidurova:countries_filling_data context:!test
-INSERT INTO public.countries(id, list_schemes_id, code)
+--changeset kateshaidurova:country_schemes_filling_data context:!test
+INSERT INTO public.country_schemes(id, list_schemes_id, code)
 VALUES (1, 1 /* ISO-ALPHA2 */, 'MD');
 
 
---changeset kateshaidurova:countries_i18n
-CREATE TABLE public.countries_i18n (
+--changeset kateshaidurova:country_schemes_i18n
+CREATE TABLE public.country_schemes_i18n (
   id                        BIGINT     NOT NULL PRIMARY KEY,
-  country_id BIGINT         NOT NULL REFERENCES countries (id),
+  country_scheme_id BIGINT         NOT NULL REFERENCES country_schemes (id),
   language_code             VARCHAR(2) NOT NULL,
   description               TEXT       NOT NULL,
-  UNIQUE (country_id, language_code)
+  UNIQUE (country_scheme_id, language_code)
 );
 
---changeset kateshaidurova:countries_i18n_filling_data context:!test
-INSERT INTO public.countries_i18n(id, country_id, language_code, description)
+--changeset kateshaidurova:country_schemes_i18n_filling_data context:!test
+INSERT INTO public.country_schemes_i18n(id, country_scheme_id, language_code, description)
 VALUES (1, 1, 'EN', 'MOLDOVA (REPUBLIC OF)'),
        (2, 1, 'RO', 'MOLDOVA'),
        (3, 1, 'RU', 'МОЛДОВА');
