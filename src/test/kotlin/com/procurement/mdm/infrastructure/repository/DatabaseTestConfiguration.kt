09329b8c1102b36a3ea339e80c2e0246945b5226
katherine <kate.shaidurova@gmail.com>
2020-06-10 20:51:13 +0300

    ES-3433 Added CountrySchemeRepository bean to DatabaseTestConfiguration.kt

--- src/test/kotlin/com/procurement/mdm/infrastructure/repository/DatabaseTestConfiguration.kt
+++ src/test/kotlin/com/procurement/mdm/infrastructure/repository/DatabaseTestConfiguration.kt
@@ -6,12 +6,14 @@ import com.procurement.mdm.domain.repository.address.AddressLocalityRepository
 import com.procurement.mdm.domain.repository.address.AddressRegionRepository
 import com.procurement.mdm.domain.repository.organization.OrganizationScaleRepository
 import com.procurement.mdm.domain.repository.organization.OrganizationSchemeRepository
+import com.procurement.mdm.domain.repository.scheme.CountrySchemeRepository
 import com.procurement.mdm.infrastructure.repository.address.PostgresAddressCountryRepository
 import com.procurement.mdm.infrastructure.repository.address.PostgresAddressLocalityRepository
 import com.procurement.mdm.infrastructure.repository.address.PostgresAddressRegionRepository
 import com.procurement.mdm.infrastructure.repository.language.PostgresLanguageRepository
 import com.procurement.mdm.infrastructure.repository.organization.PostgresOrganizationScaleRepository
 import com.procurement.mdm.infrastructure.repository.organization.PostgresOrganizationSchemeRepository
+import com.procurement.mdm.infrastructure.repository.scheme.PostgresCountrySchemeRepository
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import liquibase.Liquibase
@@ -81,6 +83,10 @@ class DatabaseTestConfiguration {
     @Bean
     fun organizationScaleRepository(): OrganizationScaleRepository =
         PostgresOrganizationScaleRepository(jdbcTemplate())
+
+    @Bean
+    fun countrySchemeRepository(): CountrySchemeRepository =
+        PostgresCountrySchemeRepository(jdbcTemplate())
 
     @Bean
     fun liquibase() = Liquibase(changeLogFile, FileSystemResourceAccessor(liquibaseDir()), database(dataSource()))
