2b6a181a98f345190300238a207dfd329e7a5a0a
katherine <kate.shaidurova@gmail.com>
2020-06-11 16:34:33 +0300

    ES-3434 Added two cases for handling request with and without scheme to AddressRegionService.kt

--- src/main/kotlin/com/procurement/mdm/application/service/address/AddressRegionService.kt
+++ src/main/kotlin/com/procurement/mdm/application/service/address/AddressRegionService.kt
@@ -1,36 +1,51 @@
 package com.procurement.mdm.application.service.address
 
+import com.procurement.mdm.application.exception.IdNotFoundException
 import com.procurement.mdm.application.exception.RegionNotFoundException
+import com.procurement.mdm.application.exception.RegionNotLinkedToCountry
 import com.procurement.mdm.domain.model.code.CountryCode
 import com.procurement.mdm.domain.model.code.LanguageCode
 import com.procurement.mdm.domain.model.code.RegionCode
 import com.procurement.mdm.domain.model.identifier.RegionIdentifier
+import com.procurement.mdm.domain.model.scheme.RegionScheme
 import com.procurement.mdm.domain.repository.AdvancedLanguageRepository
 import com.procurement.mdm.domain.repository.address.AddressRegionRepository
+import com.procurement.mdm.domain.repository.scheme.RegionSchemeRepository
 import org.springframework.stereotype.Service
 
 interface AddressRegionService {
-    fun getBy(region: String, country: String, language: String): RegionIdentifier
+    fun getBy(region: String, country: String, language: String, scheme: String?): RegionIdentifier
 }
 
 @Service
 class AddressRegionServiceImpl(
     private val addressRegionRepository: AddressRegionRepository,
+    private val regionSchemeRepository: RegionSchemeRepository,
     private val advancedLanguageRepository: AdvancedLanguageRepository
 ) : AddressRegionService {
 
-    override fun getBy(region: String, country: String, language: String): RegionIdentifier {
+    override fun getBy(region: String, country: String, language: String, scheme: String?): RegionIdentifier {
         val countryCode = CountryCode(country)
         val regionCode = RegionCode(region)
         val languageCode = LanguageCode(language)
-            .apply {
-                validation(advancedLanguageRepository)
-            }
+
+        return if (scheme == null)
+            getByDefaultScheme(countryCode, regionCode, languageCode)
+        else
+            getByReceivedScheme(countryCode, regionCode, languageCode, scheme)
+    }
+
+    private fun getByDefaultScheme(
+        country: CountryCode, region: RegionCode, language: LanguageCode
+    ): RegionIdentifier {
+        language.apply {
+            validation(advancedLanguageRepository)
+        }
 
         val regionEntity = addressRegionRepository.findBy(
-            region = regionCode,
-            country = countryCode,
-            language = languageCode
+            region = region,
+            country = country,
+            language = language
         ) ?: throw RegionNotFoundException(region = region, country = country, language = language)
 
         return RegionIdentifier(
@@ -39,5 +54,40 @@ class AddressRegionServiceImpl(
             description = regionEntity.description,
             uri = regionEntity.uri
         )
+    }
+
+    private fun getByReceivedScheme(
+        country: CountryCode, region: RegionCode, language: LanguageCode, scheme: String
+    ): RegionIdentifier {
+        val regionScheme = RegionScheme(scheme).apply {
+            validation(regionSchemeRepository)
+        }
+
+        checkExists(region = region, scheme = regionScheme)
+        checkCountryCode(region = region, scheme = regionScheme, country = country)
+
+        val regionEntity = regionSchemeRepository.findBy(
+            region = region, scheme = regionScheme, language = language, country = country
+        )
+            ?: throw RegionNotFoundException(
+                region = region, scheme = regionScheme, country = country, language = language
+            )
+
+        return RegionIdentifier(
+            scheme = regionEntity.scheme,
+            id = regionEntity.id,
+            description = regionEntity.description,
+            uri = regionEntity.uri
+        )
+    }
+
+    private fun checkExists(region: RegionCode, scheme: RegionScheme) {
+        if (regionSchemeRepository.existsBy(region = region, scheme = scheme).not())
+            throw IdNotFoundException(region = region, scheme = scheme)
+    }
+
+    private fun checkCountryCode(region: RegionCode, scheme: RegionScheme, country: CountryCode) {
+        if (regionSchemeRepository.existsBy(region = region, scheme = scheme, country = country).not())
+            throw RegionNotLinkedToCountry(region = region, scheme = scheme, country = country)
     }
 }
