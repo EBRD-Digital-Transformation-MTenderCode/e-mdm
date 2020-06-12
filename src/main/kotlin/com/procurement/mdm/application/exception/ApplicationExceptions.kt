3d2331d53e511af7e96d22c5e46dcf2b04b9fb26
katherine <kate.shaidurova@gmail.com>
2020-06-12 15:45:14 +0300

    ES-3435 Added exceptions for locality issues to ApplicationExceptions.kt

--- src/main/kotlin/com/procurement/mdm/application/exception/ApplicationExceptions.kt
+++ src/main/kotlin/com/procurement/mdm/application/exception/ApplicationExceptions.kt
@@ -2,8 +2,10 @@ package com.procurement.mdm.application.exception
 
 import com.procurement.mdm.domain.model.code.CountryCode
 import com.procurement.mdm.domain.model.code.LanguageCode
+import com.procurement.mdm.domain.model.code.LocalityCode
 import com.procurement.mdm.domain.model.code.RegionCode
 import com.procurement.mdm.domain.model.scheme.CountryScheme
+import com.procurement.mdm.domain.model.scheme.LocalityScheme
 import com.procurement.mdm.domain.model.scheme.RegionScheme
 
 sealed class ApplicationException(val description: String) : RuntimeException(description)
@@ -24,11 +26,30 @@ class RegionNotFoundException : ApplicationException {
         super("The region by code '$region', scheme '$scheme', country '$country', language '$language' not found.")
 }
 
-class RegionNotLinkedToCountryException(region: RegionCode, scheme: RegionScheme, country: CountryCode) :
-    ApplicationException("The region by code '$region' and scheme '$scheme' is not linked to country '$country'.")
+class RegionNotLinkedToCountryException : ApplicationException {
+    constructor(region: RegionCode, scheme: RegionScheme, country: CountryCode) :
+        super("The region by code '$region' and scheme '$scheme' is not linked to country '$country'.")
 
-class LocalityNotFoundException(locality: String, country: String, region: String, language: String) :
-    ApplicationException("The locality by code '$locality', country '$country', region '$region', language '$language' not found.")
+    constructor(region: RegionCode, country: CountryCode) :
+        super("The region by code '$region' is not linked to country '$country'.")
+}
+
+class LocalityNotFoundException : ApplicationException {
+    constructor(locality: LocalityCode, country: CountryCode, region: RegionCode, language: LanguageCode) :
+        super("The locality by code '$locality', country '$country', region '$region', language '$language' not found.")
+
+    constructor(
+        locality: LocalityCode,
+        scheme: LocalityScheme,
+        country: CountryCode,
+        region: RegionCode,
+        language: LanguageCode
+    ) :
+        super("The locality by code '$locality', scheme $scheme, country '$country', region '$region', language '$language' not found.")
+}
+
+class LocalityNotLinkedToRegionException(locality: LocalityCode, scheme: LocalityScheme, region: RegionCode) :
+    ApplicationException("The locality by code '$locality' and scheme '$scheme' is not linked to region '$region'.")
 
 class OrganizationSchemeNotFoundException(country: String) :
     ApplicationException("The organization schemes for country '$country' not found.")
@@ -36,17 +57,25 @@ class OrganizationSchemeNotFoundException(country: String) :
 class OrganizationScaleNotFoundException(country: String) :
     ApplicationException("The organization scale for country '$country' not found.")
 
-class SchemeNotFoundException: ApplicationException{
+class SchemeNotFoundException : ApplicationException {
     constructor (scheme: CountryScheme) :
         super("Country scheme '$scheme' not found.")
+
     constructor (scheme: RegionScheme) :
         super("Region scheme '$scheme' not found.")
+
+    constructor (scheme: LocalityScheme) :
+        super("Locality scheme '$scheme' not found.")
 }
 
-class IdNotFoundException : ApplicationException{
+class IdNotFoundException : ApplicationException {
     constructor(country: CountryCode, scheme: CountryScheme) :
         super("Country id '$country' by scheme '$scheme' not found.")
+
     constructor(region: RegionCode, scheme: RegionScheme) :
         super("Region id '$region' by scheme '$scheme' not found.")
+
+    constructor(locality: LocalityCode, scheme: LocalityScheme) :
+        super("Locality id '$locality' by scheme '$scheme' not found.")
 }
 
