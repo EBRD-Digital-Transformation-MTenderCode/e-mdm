eee2749c1b7670bcbad2c76aa043c8f1f07438ec
katherine <kate.shaidurova@gmail.com>
2020-06-16 15:29:05 +0300

    ES-3492 Added criterion_init_data.sql

---
+++ src/test/resources/sql/criteria/criterion_init_data.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO public.criterion (id, country_code, pmd, phase, code)
+VALUES (1, 'MD', 'OT', 'SUBMISSION', 'MD_OT_1'),
+       (2, 'MD', 'OT', 'SUBMISSION', 'MD_OT_2');
+
+
+INSERT INTO public.criterion_i18n (id, criteria_id, language_code, title, description)
+VALUES (1, 1, 'EN', 'criteria-title-1', 'criteria-description-1'),
+       (2, 2, 'EN', 'criteria-title-2', 'criteria-description-2');
+
+
+
+
