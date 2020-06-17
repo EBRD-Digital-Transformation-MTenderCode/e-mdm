c3a6ce83bd4f56f9f456f2a8a0fd0bf850d030f9
katherine <kate.shaidurova@gmail.com>
2020-06-17 17:02:46 +0300

    ES-3493 Added RequirementGroup to ModelDescription.kt and optional FieldDescriptor generation method

--- src/test/kotlin/com/procurement/mdm/infrastructure/web/controller/documentation/ModelDescription.kt
+++ src/test/kotlin/com/procurement/mdm/infrastructure/web/controller/documentation/ModelDescription.kt
@@ -74,11 +74,13 @@ object ModelDescription {
                     getFieldDescriptor("data[].uri", "A URI to identify the locality.")
                 )
             }
+
             fun emptyCollection(): List<FieldDescriptor> {
                 return listOf(
                     getFieldDescriptor("data", "Empty data of response.")
                 )
             }
+
             fun schemesCollection(): List<FieldDescriptor> {
                 return listOf(
                     getFieldDescriptor("data", "The data of response."),
@@ -135,7 +137,7 @@ object ModelDescription {
         }
     }
 
-    object Criteria{
+    object Criteria {
         fun collection(): List<FieldDescriptor> {
             return listOf(
                 getFieldDescriptor("data[]", "The data of response."),
@@ -144,6 +146,23 @@ object ModelDescription {
                 getFieldDescriptor("data[].title", "The title of the criteria.")
             )
         }
+
+        fun emptyCollection(): List<FieldDescriptor> {
+            return listOf(
+                getFieldDescriptor("data", "Empty data of response.")
+            )
+        }
+    }
+
+    object RequirementGroup {
+        fun collection(): List<FieldDescriptor> {
+            return listOf(
+                getFieldDescriptor("data[]", "The data of response."),
+                getFieldDescriptor("data[].id", "The identifier of the requirement group."),
+                getFieldDescriptorOptional("data[].description", "The description of the requirement group.")
+            )
+        }
+
         fun emptyCollection(): List<FieldDescriptor> {
             return listOf(
                 getFieldDescriptor("data", "Empty data of response.")
@@ -162,3 +181,6 @@ object ModelDescription {
 
 private fun getFieldDescriptor(property: String, description: String): FieldDescriptor =
     fieldWithPath(property).description(description)
+
+private fun getFieldDescriptorOptional(property: String, description: String): FieldDescriptor =
+    fieldWithPath(property).description(description).optional().type(String::class.java)
