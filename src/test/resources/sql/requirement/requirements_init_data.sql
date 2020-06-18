cc7b463cdb5005a05a1cc83792286d25e12fb320
katherine <kate.shaidurova@gmail.com>
2020-06-18 14:15:57 +0300

    ES-3494 Added insert to requirements_init_data.sql

--- src/test/resources/sql/requirement/requirements_init_data.sql
+++ src/test/resources/sql/requirement/requirements_init_data.sql
@@ -1,7 +1,8 @@
 INSERT INTO public.requirements (id, country_code, pmd, phase, requirement_group_code, code)
 VALUES (1, 'MD', 'OT', 'SUBMISSION', 'REQ_GROUP_1', 'REQ_1'),
        (2, 'MD', 'OT', 'SUBMISSION', 'REQ_GROUP_1', 'REQ_1_1'),
-       (3, 'MD', 'OT', 'SUBMISSION', 'REQ_GROUP_2', 'REQ_2');
+       (3, 'MD', 'OT', 'SUBMISSION', 'REQ_GROUP_2', 'REQ_2'),
+       (4, 'MD', 'OT', 'SUBMISSION', 'REQ_GROUP_3', 'REQ_3');
 
 
 INSERT INTO public.requirements_i18n (id, requirement_id, language_code, title, description)
