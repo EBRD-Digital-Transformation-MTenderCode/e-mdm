49abb4b2962264cc68461585db8f9f35334a29cb
Evgen Kopach <zkopach@gmail.com>
2020-10-20 20:15:57 +0300

    ES-4486 Created controller and service for handling /units endpoint.

---
+++ src/main/kotlin/com/procurement/mdm/application/service/unit/UnitService.kt
@@ -0,0 +1,50 @@
+package com.procurement.mdm.application.service.unit
+
+import com.procurement.mdm.application.exception.UnitLanguageNotFoundException
+import com.procurement.mdm.application.exception.UnitNotFoundException
+import com.procurement.mdm.application.exception.UnitTranslationNotFoundException
+import com.procurement.mdm.application.service.unit.UnitServiceImpl.UnitById.unitByLangFilter
+import com.procurement.mdm.domain.model.code.LanguageCode
+import com.procurement.mdm.domain.model.unit.Unit
+import com.procurement.mdm.domain.repository.AdvancedLanguageRepository
+import com.procurement.mdm.model.entity.Units
+import com.procurement.mdm.repository.UnitRepository
+import org.springframework.stereotype.Service
+
+interface UnitService {
+    fun getById(id: String, language: String): Unit
+}
+
+@Service
+class UnitServiceImpl(
+    private val advancedLanguageRepository: AdvancedLanguageRepository,
+    private val unitRepository: UnitRepository
+) : UnitService {
+
+    override fun getById(id: String, language: String): Unit {
+        val languageCode = LanguageCode(language)
+            .apply { validation(this) }
+
+        val storedUnits = unitRepository.findByUnitKeyCode(code = id)
+        return if (storedUnits.isEmpty())
+            throw UnitNotFoundException(id = id)
+        else
+            storedUnits
+                .find { unit -> unitByLangFilter(unit, languageCode) }
+                ?.let { unit -> Unit(id = unit.unitKey!!.code!!, name = unit.name) }
+                ?: throw UnitTranslationNotFoundException(language = id)
+    }
+
+    private fun validation(languageCode: LanguageCode) {
+        if (advancedLanguageRepository.exists(languageCode).not())
+            throw UnitLanguageNotFoundException(language = languageCode)
+    }
+
+    private object UnitById {
+
+        fun unitByLangFilter(storedUnit: Units, targetLang: LanguageCode): Boolean {
+            return storedUnit.unitKey!!.language!!.code.toUpperCase() == targetLang.value.toUpperCase()
+        }
+    }
+}
+
---
+++ src/main/kotlin/com/procurement/mdm/infrastructure/web/controller/unit/UnitController.kt
@@ -0,0 +1,39 @@
+package com.procurement.mdm.infrastructure.web.controller.unit
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.procurement.mdm.application.service.unit.UnitService
+import com.procurement.mdm.infrastructure.exception.LanguageRequestParameterMissingException
+import com.procurement.mdm.infrastructure.web.dto.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/units")
+class UnitController(private val unitService: UnitService) {
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getClassificationById(
+        @RequestParam(value = "lang", required = false) lang: String?,
+        @PathVariable(value = "id") id: String
+    ): ClassificationApiResponse {
+        if (lang == null)
+            throw LanguageRequestParameterMissingException()
+
+        return unitService.getById(id = id, language = lang)
+            .let { unit -> Unit(id = unit.id, name = unit.name) }
+            .let { ClassificationApiResponse(it) }
+    }
+
+    class ClassificationApiResponse(unit: Unit) : ApiResponse<Unit>(unit)
+
+    data class Unit(
+        @field:JsonProperty("id") @param:JsonProperty("id") val id: String,
+        @field:JsonProperty("name") @param:JsonProperty("name") val name: String
+    )
+}
