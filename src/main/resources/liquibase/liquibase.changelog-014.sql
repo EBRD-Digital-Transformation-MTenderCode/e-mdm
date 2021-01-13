6b76d0a9f5aa0268b8134381c7fef14ace2866ea
Evgen Kopach <zkopach@gmail.com>
2021-01-13 14:39:57 +0200

    ES-5232 Refactored sql scripts

--- src/main/resources/liquibase/liquibase.changelog-014.sql
+++ src/main/resources/liquibase/liquibase.changelog-014.sql
@@ -3,8 +3,8 @@
 --changeset kopach:standard_criteria
 CREATE TABLE public.standard_criteria
 (
-    id            TEXT PRIMARY KEY,
+    id            TEXT       NOT NULL PRIMARY KEY,
     country_code  VARCHAR(2) NOT NULL,
     language_code VARCHAR(2) NOT NULL,
-    data          jsonb NOT NULL
+    data          jsonb      NOT NULL
 );
